41a18f66236613d2545275ba7dfdfa611956cb24
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2022-04-17 22:24:00 -0400

    Create gradient from red to black
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <fstream>
 
+// The maximum value for a given color
+// TODO: find the right terminology for this - sub-pixel?
+constexpr int CMAX = 255;
+
+// Initialize a "Portable Pixmap" file in "P3" mode, i.e. ASCII input with full
+// color support
+void init_ppm(std::ostream& out, int width, int height) {
+  out << "P3\n" << width << " " << height << '\n' << CMAX << '\n';
+}
+
+struct Color {
+  int red = 0;
+  int green = 0; 
+  int blue = 0;
+};
+
+// Every pixel will be on its own line
+void write_pixel(std::ostream& out, const Color& color) {
+  out << color.red << ' ' << color.green << ' ' << color.blue << '\n';
+}
+
 int main () {
+  const int width = 256;
+  const int height = 256;
+
   std::ofstream outfile("test.ppm", std::ios::out);
-  outfile << "Hello, Ray Tracing in a weekend!\n";
+  init_ppm(outfile, width, height);
+
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      Color color;
+
+      color.red = CMAX - i;
+      write_pixel(outfile, color);
+    }
+  }
 }
