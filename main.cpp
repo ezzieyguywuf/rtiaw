42f9ef09b06c0dae9156f84c52c191a2aa04b4a7
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2022-04-17 23:22:01 -0400

    Basic queue functionality
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
 
 // The maximum value for a given color channel
 constexpr int CMAX = 255;
@@ -20,9 +24,39 @@ void write_pixel(std::ostream& out, const Color& color) {
   out << color.red << ' ' << color.green << ' ' << color.blue << '\n';
 }
 
+// Logs the data in the fifo to std::cout, using ANSI escape sequences to reuse
+// the same area of the terminal rather than scrolling
+void log(const std::vector<std::string>& data) {
+  std::cout << "logging...\n";
+  for (const std::string& d : data) {
+    std::cout << d << '\n';
+  }
+  std::cout << "...logged\n";
+}
+
+// A very rudimentary "queue" - when the vector reaches the target size, the
+// first element will be removed, everything will be shifted "up", and finally
+// the target item is added to the back
+void push(
+  std::vector<std::string>& data, const std::string& item, std::size_t n = 10) {
+  if (data.size() < n) {
+    data.push_back(item);
+    return;
+  }
+
+  for (std::size_t i = 0; i < n - 1; ++i) {
+    data[i] = data[i + 1];
+  }
+  data[n - 1] = item;
+}
+
 int main () {
-  const int width = 256;
-  const int height = 256;
+  const int width = 5;
+  const int height = 5;
+
+  std::vector<std::string> log_data;
+  std::queue<std::string, std::vector<std::string>> log_queue(log_data);
+
 
   std::ofstream outfile("test.ppm", std::ios::out);
   init_ppm(outfile, width, height);
@@ -36,6 +70,10 @@ int main () {
       color.blue = 255 / 4;
 
       write_pixel(outfile, color);
+
+      std::string data = "Row: " + std::to_string(j) + ", Col: " + std::to_string(i);
+      push(log_data, data, 4);
+      log(log_data);
     }
   }
 }
