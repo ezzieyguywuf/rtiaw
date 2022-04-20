8235098b9c511a11f90bc62bd8adfad986e8b25f
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2022-04-19 21:35:33 -0400

    We're making a real sphere now 🥳
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <chrono>
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <optional>
 #include <queue>
 #include <string>
+#include <sstream>
 #include <thread>
 #include <vector>
 
@@ -31,6 +34,12 @@ struct Vector {
     return {dx * val, dy * val, dz * val};
   }
 };
+
+std::ostream& operator<<(std::ostream& out, const Vector& vec) {
+  out << "{" << vec.dx << ", " << vec.dy << ", " << vec.dz << "}";
+  return out;
+}
+
 
 Vector operator* (int val, const Vector& vec) {
   return vec * val;
@@ -108,6 +117,12 @@ struct Ray {
   Vector direction;
 };
 
+std::ostream& operator<<(std::ostream& out, const Ray& ray) {
+  out << "Ray{origin: " << ray.origin << ", " 
+      <<     "direction:" << ray.direction << "}";
+  return out;
+}
+
 // General equation for a sphere: (x - Cx)² + (y - Cy)² + (z - Cz)² = r²
 // in vector form, C = {Cx, Cy, Cz}, P = {x, y, z}, so (P - C) · (P - C) = r²
 //
@@ -128,7 +143,7 @@ struct Ray {
 //
 // (P - C) · (P - C) = (A + t·B - C) · (A + t·B - C)
 //                   = (B·B)t² - 2(B·(A - C))t + ((A-C)·(A-C) - r²) = 0
-bool hit_sphere(const Ray& ray, const Sphere& sphere) {
+bool hit_sphere(const Ray& ray, const Sphere& sphere, std::optional<std::reference_wrapper<std::ostream>> ostream = std::nullopt) {
   // (A - C) in equations above
   Vector ca{ray.origin.dx - sphere.center.dx,
             ray.origin.dy - sphere.center.dy,
@@ -137,14 +152,25 @@ bool hit_sphere(const Ray& ray, const Sphere& sphere) {
   double b = 2 * dot(ca, ray.direction);
   double c = dot(ca, ca) - (sphere.radius * sphere.radius);
 
+  if (ostream) {
+    ostream.value().get() << "  ca: " << ca << '\n'
+             << "    a: " << a << '\n'
+             << "    b: " << b << '\n'
+             << "    c: " << c << '\n';
+  }
+
   return b*b - 4*a*c > 0;
 }
 
 // TODO: would a cache help here? e.g. memoization
 // TODO: add origin (for moving the camera later)
-Color ray_color(const Ray& ray) {
+Color ray_color(const Ray& ray, std::optional<std::reference_wrapper<std::ostream>> ostream = std::nullopt) {
   // 0 < t < 1
-  double t = ray.direction.dy / vector_length(ray.direction);
+  double t = 0.5 * ray.direction.dy / vector_length(ray.direction);
+
+  if (ostream) {
+    ostream.value().get() << "  for vector: " << ray.direction << ", length: " << vector_length(ray.direction) << '\n';
+  }
 
   int red = CMAX * lerp(0.5, 1.0, t);
   int green = CMAX * lerp(0.7, 1.0, t);
@@ -155,56 +181,75 @@ Color ray_color(const Ray& ray) {
 
 int main () {
   // image
-  double aspect_ratio = 16.0 / 9.0;
+  const double aspect_ratio = 16.0 / 9.0;
   const int height = 600;
+  /* double aspect_ratio = 1.0; */
+  /* int height = 2; */
   const int width = height * aspect_ratio;
 
   // Camera
   //
-  // Our viewport is scaled down to 0 < y < 4
+  // Our viewport is scaled down to  -2 < y < 2
   // x is fixed by the apsect ratio, which is width / height, or x / y
-  // therefor, 0 < x < 4 * 16 / 9
-  // or approx. 0 < x < 7.11
+  // therefor, -2 * 16/9 < x < 2 * 16/9
+  // or approx. -3.55 < x < 3.55
   // 
   // x is positive to the right
   // y is positive down
   //
-  // Top-left of the screen is x = 0, y = 0
-  // bottom-right of the screen is x = width, y = height
+  // Top-left of the screen is x = -width/2, y = -height/2 
+  // bottom-right of the screen is x = width/2, y = height/2
   // positive Z is into the screen
   double viewport_height = 4.0;
   double viewport_width = viewport_height * aspect_ratio;
   double focal_length = 1.0;
 
-  const std::size_t log_rows = 4;
+  std::cout << "height: " << height << ", width: " << width << '\n';
+  std::cout << "vp_height: " << viewport_height << ", vp_width: " << viewport_width << '\n';
+
+  const std::size_t log_rows = 10;
   std::vector<std::string> log_data;
   std::queue<std::string, std::vector<std::string>> log_queue(log_data);
   Logger logger(log_rows);
 
   std::ofstream outfile("test.ppm", std::ios::out);
+  std::ofstream logfile("log.txt", std::ios::out);
   init_ppm(outfile, width, height);
 
-  Sphere sphere{Vector{viewport_width/2.0, viewport_height/2.0, focal_length}, 0.5};
+  Vector viewport_center{0.0, 0.0, 0.0};
+
+  Sphere sphere{Vector{viewport_center.dx, viewport_center.dy, focal_length}, 0.5};
+  logfile << "width: " << width
+          << ", height: " << height
+          << ", aspect_ratio: " << aspect_ratio <<'\n';
+  logfile << "viewport_width: " << viewport_width
+          << ", viewport_height: " << viewport_height << '\n';
+  logfile << "Sphere center: " << sphere.center << ", rad: " << 0.5 << '\n';
 
   // i and j represent one pixel each along the +x and +y axes, respectively.
   for (int j = 0; j < height; ++j) {
     for (int i = 0; i < width; ++i) {
-      double x = lerp(0, viewport_width, i / double(width));
-      double y = lerp(0, viewport_height, j / double(height));
+      double x = lerp(-viewport_width, viewport_width, i / double(width));
+      double y = lerp(-viewport_height, viewport_height, j / double(height));
       double z = focal_length;
 
-      Ray ray{/*origin=*/{0,0,0}, /*direction=*/{x, y, z}};
+      Ray ray{/*origin=*/{viewport_center.dx, viewport_center.dy, -focal_length}, /*direction=*/{x, y, z}};
 
+      /* logfile << "Row: " << j << ", Col: " << i << ", " << ray; */
+      /* logfile << "  x: " << x << ", y: " << y << ", z: " << z << '\n'; */
       if (hit_sphere(ray, sphere)) {
         write_pixel(outfile, {CMAX, 0, 0});
+        /* logfile << "  HIT!\n"; */
       } else {
         Color color = ray_color(ray);
         write_pixel(outfile, color);
+        /* logfile << "  MISS!\n"; */
       }
 
-      /* std::string data = "Row: " + std::to_string(j) + ", Col: " + std::to_string(i); */
-      /* push(log_data, data, log_rows); */
+      /* std::ostringstream data; */
+      /* data << "Row: " << j << ", Col: " << i << ", " << ray; */
+      /* push(log_data, data.str(), log_rows); */
+      /* logger.log(log_data); */
     }
-    /* logger.log(log_data); */
   }
 }
