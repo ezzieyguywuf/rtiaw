e3f1f7489fed8ce3488747016fbf4211ccd4ac81
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2022-05-29 22:04:11 -0400

    Draw random colored vertical bar in rust
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- rtiaw-rust/src/main.rs
+++ rtiaw-rust/src/main.rs
@@ -1,4 +1,6 @@
+use rand::prelude::*;
 use softbuffer::GraphicsContext;
+use std::time::Instant;
 use winit::{
     dpi::PhysicalSize,
     event::{Event, WindowEvent},
@@ -18,10 +20,22 @@ impl Color {
     }
 }
 
+// Top-left is row = 0, col = 0
+struct Position {
+    row: usize,
+    col: usize,
+}
+
+struct Pixel {
+    color: Color,
+    location: Position,
+}
+
 // Per the Softbuffer requirements[1], each pixel is represented by 32 bits
 // [1]: https://docs.rs/softbuffer/latest/softbuffer/struct.GraphicsContext.html#method.set_buffer
 struct Framebuffer {
     pixels: Vec<u32>,
+    width: usize,
 }
 
 impl Framebuffer {
@@ -32,12 +46,20 @@ impl Framebuffer {
             blue: 230 as u8,
         };
 
-        let mut buffer = Framebuffer { pixels: Vec::new() };
+        let mut buffer = Framebuffer {
+            pixels: Vec::new(),
+            width,
+        };
 
         buffer.pixels = Vec::with_capacity(width * height);
         buffer.pixels.resize(width * height, background.to_bytes());
 
         buffer
+    }
+
+    fn set(&mut self, pixel: &Pixel) {
+        let i = pixel.location.row * self.width + pixel.location.col;
+        self.pixels[i] = pixel.color.to_bytes();
     }
 }
 
@@ -53,11 +75,32 @@ fn main() {
         .build(&event_loop)
         .unwrap();
     let mut graphics_context = unsafe { GraphicsContext::new(window) }.unwrap();
-    let buffer = Framebuffer::new(WINDOW_WIDTH as usize, WINDOW_HEIGHT as usize);
+    let mut buffer = Framebuffer::new(WINDOW_WIDTH as usize, WINDOW_HEIGHT as usize);
+    let mut col = 0;
+    let mut rng = rand::thread_rng();
+    let mut clock = Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
+        let red: u8 = rng.gen_range(0..255);
+        let green: u8 = rng.gen_range(0..255);
+        let blue: u8 = rng.gen_range(0..255);
+        for row in 0..WINDOW_HEIGHT {
+            buffer.set(&Pixel {
+                color: Color { red, green, blue },
+                location: Position {
+                    row: row as usize,
+                    col: col as usize,
+                },
+            })
+        }
+        col += 1;
+        if col >= WINDOW_WIDTH {
+            col = 0;
+        }
+
+        clock = Instant::now();
         match event {
             Event::RedrawRequested(window_id) if window_id == graphics_context.window().id() => {
                 graphics_context.set_buffer(
@@ -65,6 +108,9 @@ fn main() {
                     WINDOW_WIDTH as u16,
                     WINDOW_HEIGHT as u16,
                 );
+            }
+            Event::MainEventsCleared => {
+                graphics_context.window().request_redraw();
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
